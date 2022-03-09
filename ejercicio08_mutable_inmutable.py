c811aa7d2929317b1d3a021cd77a7295d0e7f084
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-03-09 11:44:37 -0600

    feat: 🎸 estructura mutables e inmutables

---
+++ ejercicio08_mutable_inmutable.py
@@ -0,0 +1,27 @@
+
+'''
+notas = [6 , 9 , 3 , 8 , 10]
+#lista mutable
+print(notas[2])
+print("valor inicial ", notas[2])
+notas[2] = 5
+print("valor final", notas[2])
+print(notas)
+
+'''
+
+#String (Inmutable)---------------------------------------------------------------------------
+
+'''
+curso="python y django de cero"
+print(curso[1])
+curso[1]="Y"
+
+'''
+
+
+#Tublas (Inmutable, su valor no puede cambiar )------------------------------------------------------
+notas = (6 , 9 , 3 , 8 , 10)
+#notas[2]=43
+print(notas[2])
+
---
+++ ejercico7_lista.py
@@ -0,0 +1,32 @@
+personas=["enrique", "enrique","Jose", "Eduardo", "Luzmila","Carmen"]
+
+notas = [6 , 9 , 3 , 8 , 10]
+
+lista1 = ["Hola", 13, 19.9, True]
+
+
+# saca a jose y eduardo
+#print(personas[1:4])
+
+
+#insertar un valor
+'''
+personas.insert(1, 'Diana')
+print(personas)
+'''
+
+#Obtener la cantidad de elementos
+#print(len(personas))
+
+#cuantas veces se repite enrique
+#print(personas.count('enrique'))
+
+
+
+
+
+
+
+
+
+
