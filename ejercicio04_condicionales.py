fd79432dad0c86eec095b9bde16c3a1ac4325993
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-03-09 10:02:57 -0600

    feat: 🎸 condicionales en python

---
+++ ejercicio04_condicionales.py
@@ -0,0 +1,49 @@
+'''
+
+edad =int(input("ingresa tu edad\n"))
+if edad == 15:
+    print("tu edad es 15")
+else:
+    print("Tu edad no es 15")
+
+'''
+
+
+'''
+# H (es hombre) M ( es mujer)
+caracter = input("Ingrese H si es hombre y M si es mujer\n")
+if caracter == "M":
+    print("Es mujer")
+else:
+    if caracter == "H":
+        print("Es hombre")
+
+'''
+
+
+# H (es hombre) M ( es mujer)
+caracter = input("Ingrese H si es hombre y M si es mujer\n")
+if caracter == "M":
+    print("Es mujer")
+elif caracter == "H":
+    print("Es hombre")
+else:
+    print("caracter no valido man")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
