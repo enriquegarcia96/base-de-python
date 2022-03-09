bec43665e11ae3ac24c6d4d210a78a3f88a7975b
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-03-08 20:47:38 -0600

    feat: 🎸 bases de python

---
+++ main.py
@@ -0,0 +1,21 @@
+#Este es un comentario de una linea
+
+'''
+Este
+es un
+comentario
+multilinea
+'''
+
+
+'''
+print("hola mundo")
+print("Hola a todos")
+'''
+
+
+
+# Definir variables
+nombre='Enrique'
+apellido='García'
+print(nombre, apellido)
