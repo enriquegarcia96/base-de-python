0084a5ca5012d9b200b0a35a68e7082e1936be51
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-03-09 12:08:54 -0600

    feat: 🎸 Funciones en python

---
+++ Funciones.py
@@ -0,0 +1,30 @@
+def saludo():
+    print("hola mundo")
+
+
+saludo()
+saludo()
+saludo()
+
+
+def suma(n1, n2):
+    return n1 + n2
+
+print( suma(1,5) )
+print( suma(5,33) )
+
+def saludo_mensaje(nombre):
+    print("hola mundo ",nombre)
+
+saludo_mensaje('kike')
+saludo_mensaje('santos')
+
+
+def saludos_mensaje(nombre='enrique', apellido='romero'):
+    print("hola mundo ",nombre, " ", apellido)
+
+
+saludos_mensaje("snato", 'quiuqe')
+
+
+
